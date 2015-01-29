1ebd3c55d37f4e691bdf70835d2a64420ed2b87b
dighteach <dighteach@gmail.com>
2015-01-28 20:16:25 -0600

    added 402 week 3 SQL

---
+++ 2015/DIGH402/week3/402week3.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 28, 2015 at 07:50 PM
+-- Server version: 5.5.29
+-- PHP Version: 5.3.10-1ubuntu3.15
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `402week3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content`
+--
+
+CREATE TABLE IF NOT EXISTS `content` (
+  `contentid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `contentname` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `contentdesc` text COLLATE utf8_unicode_ci NOT NULL,
+  `contenttext` text COLLATE utf8_unicode_ci NOT NULL,
+  `contentimage` text COLLATE utf8_unicode_ci NOT NULL,
+  `contentcreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`contentid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content_lookup`
+--
+
+CREATE TABLE IF NOT EXISTS `content_lookup` (
+  `content_id` int(10) unsigned NOT NULL,
+  `content_type_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`content_id`,`content_type_id`,`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content_type`
+--
+
+CREATE TABLE IF NOT EXISTS `content_type` (
+  `content_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content_type_name` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `content_type_desc` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`content_type_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `content_type`
+--
+
+INSERT INTO `content_type` (`content_type_id`, `content_type_name`, `content_type_desc`) VALUES
+(1, 'text', 'content defined as textual or text based'),
+(2, 'image', 'content defined as image or visually based'),
+(3, 'audio', 'content defined as audio based for output and playback'),
+(4, 'video', 'content defined as video based for output and playback'),
+(5, 'bibliography', 'content defined as a bibliographical record or data'),
+(6, 'tei', 'textual material marked up using the XML schema TEI P5'),
+(7, 'map', 'map coordinates etc for a Google Maps API rendering');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `userid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `usercreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `username`, `firstname`, `lastname`, `usercreated`) VALUES
+(1, 'ancientlives', 'ancient', 'lives', '2014-01-22 05:37:30'),
+(2, 'yvaine08', 'yvaine', 'wall', '2014-01-22 05:42:36'),
+(3, 'yvaine14', 'yvaine', 'stormhold', '2014-01-22 05:42:49'),
+(4, 'tristan27', 'tristan', 'issit', '2014-01-22 05:44:03'),
+(5, 'emma97', 'emma', 'bernau', '2014-01-22 22:58:09'),
+(6, 'cat05', 'catherine', 'smith', '2014-01-22 22:58:09'),
+(7, 'amelie01', 'amelie', 'poulain', '2014-01-27 22:22:50'),
+(8, 'adele10', 'adele', 'blanc-sec', '2014-01-27 22:22:50');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
