93e9a877299efb8e57d134815c68789aa8cb85e4
dighteach <dighteach@gmail.com>
2015-03-24 17:48:25 -0500

    added week 11 source and sql

---
+++ 2015/DIGH402/402framework/v0.6/plugins/pager/pager.js
@@ -0,0 +1,43 @@
+/**
+ * pager.js - js for pager plugin using jquery library
+ */
+ 
+$(document).ready(function() {
+$("span#pager").hide();
+});
+ 
+$(document).ready(function() { 
+$('div#content').append('<div class="clear">&nbsp;</div><center><ul id="paging"></ul></center>');
+
+pageSize = 12;
+
+var total = $('.group_item').length;
+var pages = Math.ceil(total/pageSize);
+
+$('div#content').prepend('<div class="group_counter"><p>Total items: '+total+'</p></div>');
+
+for ( var i = 1; i < pages+1; i++ ) {
+if (i == 1) {
+$('ul#paging').append('<li class="current" title="select page '+i+'">'+i+'</li>');
+} else {
+$('ul#paging').append('<li title="select page '+i+'">'+i+'</li>');
+}
+}
+
+showPage = function(page) {
+    $("div.group_item").hide();
+    $("div.group_item").each(function(n) {
+        if (n >= pageSize * (page - 1) && n < pageSize * page)
+            $(this).show();
+    });        
+}
+    
+showPage(1);
+
+$("#paging li").click(function() {
+    $("#paging li").removeClass("current");
+    $(this).addClass("current");
+    showPage(parseInt($(this).text())) 
+});
+
+});
